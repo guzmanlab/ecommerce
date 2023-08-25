0e30795032016a83d4c08528f137093785b87197
Edgar Guzman <gamingtop@yahoo.com>
2023-08-24 22:38:27 -0400

    feat: Add a stardard guideline for formating

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,30 +1,59 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', 'import', 'prettier', '@typescript-eslint'],
+    extends: [
+        'next',
+        'airbnb',
+        'prettier',
+        'airbnb-typescript',
+        'next/core-web-vitals',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked'
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
+    rules: {
+        // These opinionated rules are enabled in stylistic-type-checked above.
+        // Feel free to reconfigure them to your own preference.
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+
+        '@typescript-eslint/no-use-before-define': ['off', { typedefs: false }],
+
+        'no-void': ['off', { allowAsStatement: true }],
+        '@typescript-eslint/no-void': ['off', { allowAsStatement: true }],
+
+        'react/button-has-type': ['off', { allowAsStatement: true }],
+        'react/function-component-definition': 'off',
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off',
+
+        '@typescript-eslint/consistent-type-imports': [
+            'off',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports'
+            }
+        ],
+        '@typescript-eslint/no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
+
+        indent: ['off', 4],
+        '@typescript-eslint/indent': ['off', 4]
+    },
+    settings: {
+        react: {
+            detect: 'latest'
+        }
+    }
 };
 
 module.exports = config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import { api } from "@/utils/api";
-import "@/styles/globals.css";
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { type AppType } from 'next/app';
+import { api } from '@/utils/api';
+import '@/styles/globals.css';
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
+const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </SessionProvider>
-  );
-};
+);
 
 export default api.withTRPC(MyApp);
