964a221fbcbbcc8ed22d844bf9339d2b78aef325
Edgar Guzman <gamingtop@yahoo.com>
2023-08-24 22:38:27 -0400

    feat: Add a stardard guideline for formating

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,17 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    printWidth: 180,
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'avoid',
+    singleAttributePerLine: true,
+
+    plugins: [require.resolve('prettier-plugin-tailwindcss')]
 };
 
 module.exports = config;
